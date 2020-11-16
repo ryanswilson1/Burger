83b56be44c7ae256b99d30ce55270fd5830c29ea
Ryan Wilson <ryanswilson1@gmail.com>
2020-11-15 19:06:24 -0500

    created orm.js and connection.js files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+let connection;
+
+if (process.env.BURGERSDB_URL) {
+    connection = mysql.createConnection(process.env.BURGERSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.MYSQL_PASSWORD,
+        database: "burgers_db"
+    });
+}
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
